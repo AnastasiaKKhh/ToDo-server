90a5dd7671ff98aadc55d8486fd83b8261f73a9c
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-25 18:20:56 +0300

    handling errors while DELETE request

--- Routes/todos/todo.delete.js
+++ Routes/todos/todo.delete.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const { defaultError } = require("../../errors");
 const fs = require("fs").promises;
 
 const router = express.Router();
 
-router.delete("/todo/:uuid", function (req, res) {
-  const { params: { uuid } } = req;
-    fs.readFile("data.json")
-      .then((data) => {
-        const tasks = JSON.parse(data);
-        const deletedTask = tasks.find(item => item.uuid === uuid)
-        newTasks = tasks.filter((task) => task.uuid !== deletedTask.uuid)
-        fs.writeFile("data.json", JSON.stringify(newTasks));
-        console.log(deletedTask)
-        return res.send(`Task with uuid ${deletedTask.uuid} was deleted`);
-      })
-  });
+
+router.delete("/todo/:uuid", async function (req, res) {
+  try {
+  const {params: { uuid }} = req;
+  const data = await fs.readFile("data.json");
+
+  const tasks = JSON.parse(data);
+  const deletedTask = tasks.find((item) => item.uuid === uuid);
+
+  if(!deletedTask) {
+    throw defaultError(404, "Task not found")
+  }
+  newTasks = tasks.filter((task) => task.uuid !== deletedTask.uuid);
+  fs.writeFile("data.json", JSON.stringify(newTasks));
+  console.log(deletedTask);
+  return res.send(`Task with uuid ${deletedTask.uuid} was deleted`);
+} catch(error) {
+  return res.send(error)
+}
+});
 
 module.exports = router;
