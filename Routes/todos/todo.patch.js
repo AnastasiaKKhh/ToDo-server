8b9e7f0d3d68598eea2aa8df85a60a87843cd843
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-25 18:42:36 +0300

    handling errors while 'patch' request

--- Routes/todos/todo.patch.js
+++ Routes/todos/todo.patch.js
@@ -2,28 +2,51 @@ const express = require("express");
 const fs = require("fs").promises;
 
 const router = express.Router();
+const { defaultError } = require("../../errors");
 
-router.patch("/todo/:uuid", function (req, res) {
-    const { params: { uuid }, body } = req;
-  
-    fs.readFile("data.json")
-      .then((data) => {
-        const tasks = JSON.parse(data);
-        const oldTask = tasks.find(item => item.uuid === uuid)
-        const updatedTask = { ...oldTask, ...body };
-        
-        const newTasks = tasks.map((item) => {
-          if (item.uuid === uuid) {
-            const newItem = { ...item, ...body }
-            return newItem
-          }
-          return item
-        })
+router.patch("/todo/:uuid", async function (req, res) {
+  try {
+    const {params: { uuid },body,} = req;
 
-        fs.writeFile("data.json", JSON.stringify(newTasks));
-  
-        return res.send(updatedTask);
-      })
-  });
+    const regExp = /[a-zA-Z0-9]/g;
+    if (!body.name || !regExp.test(body.name)) {
+      throw defaultError(
+        422,
+        "Invalid fields in request! Try to rewrite your task"
+      );
+    }
+    if (body.name.length > 250) {
+      throw defaultError(
+        500,
+        "Too many symbols. Max is 250"
+      );
+    }
 
-  module.exports = router
+    const data = await fs.readFile("data.json");
+    const tasks = JSON.parse(data);
+    const sameTask = tasks.find((item) => item.name === body.name  && item.uuid !== uuid);
+
+    if (sameTask) {
+      throw defaultError(400, "Task not created! Maybe the same task already exists");
+    }
+    const oldTask = tasks.find((item) => item.uuid === uuid);
+    const updatedTask = { ...oldTask, ...body };
+    
+
+    const newTasks = tasks.map((item) => {
+      if (item.uuid === uuid) {
+        const newItem = { ...item, ...body };
+        return newItem;
+      }
+      return item;
+    });
+
+    fs.writeFile("data.json", JSON.stringify(newTasks));
+
+    return res.send(updatedTask);
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
+module.exports = router;
