29e92f37ffef8f49409075b6806701268345a283
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-25 18:21:54 +0300

    handling errors while 'get' request

--- Routes/todos/todos.get.js
+++ Routes/todos/todos.get.js
@@ -2,12 +2,12 @@ const c = require("config");
 const express = require("express");
 const fs = require("fs").promises;
 const router = express.Router();
+const { defaultError } = require("../../errors");
 
-router.get("/todos", function (req, res) {
-  fs.readFile("data.json").then((data) => {
+router.get("/todos", async function (req, res) {
+  try {
+    const data = await fs.readFile("data.json");
     let tasks = JSON.parse(data);
-    console.log(req.query)
-    console.log(req.params)
 
     if (req.query.order === "asc") {
       tasks = tasks.sort((prev, next) => {
@@ -20,13 +20,13 @@ router.get("/todos", function (req, res) {
       });
     }
     if (req.query.filterBy === "done") {
-      tasks = tasks.filter((task) => task.done);/////
+      tasks = tasks.filter((task) => task.done);
     }
     if (req.query.filterBy === "undone") {
       tasks = tasks.filter((task) => !task.done);
     }
     if (req.query.pp && req.query.page) {
-      const lastTaskIndex = req.query.page *  req.query.pp;
+      const lastTaskIndex = req.query.page * req.query.pp;
       const firstTaskIndex = lastTaskIndex - req.query.pp;
       tasks = tasks.slice(firstTaskIndex, lastTaskIndex);
       console.log("Tasks per page: ", req.query.pp);
@@ -34,7 +34,9 @@ router.get("/todos", function (req, res) {
     }
 
     res.send({ count: tasks.length, tasks });
-  });
+  } catch (error) {
+    return res.send(defaultError(500, "Something went wrong"));
+  }
 });
 
 module.exports = router;
