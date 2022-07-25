cd6db52bf1abc79afa614b986e98d125a5126d71
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-25 11:53:55 +0300

    fix bug with slicing

--- Routes/todos.get.js
+++ Routes/todos.get.js
@@ -1,34 +1,38 @@
+const c = require("config");
 const express = require("express");
 const fs = require("fs").promises;
 const router = express.Router();
 
-
 router.get("/todos", function (req, res) {
-  fs.readFile("data.json")
-    .then((data) => {
-      let tasks = JSON.parse(data);
-      tasks = tasks.slice(((req.query.page-1)*req.query.pp), req.query.pp*req.query.page);
-      
-      if (req.query.order === 'asc') {
-        tasks = tasks.sort((prev, next) => {
-          return new Date(prev.createdAT) - new Date(next.createdAT);
-        })
-      }
-      if (req.query.order === 'desc') {
-        tasks = tasks.sort((prev, next) => {
+  fs.readFile("data.json").then((data) => {
+    let tasks = JSON.parse(data);
 
-          return new Date(next.createdAT) - new Date(prev.createdAT);
-        })
+    if (req.query.order === "asc") {
+      tasks = tasks.sort((prev, next) => {
+        return new Date(prev.createdAT) - new Date(next.createdAT);
+      });
+    }
+    if (req.query.order === "desc") {
+      tasks = tasks.sort((prev, next) => {
+        return new Date(next.createdAT) - new Date(prev.createdAT);
+      });
+    }
+    if (req.query.filterBy === "done") {
+      tasks = tasks.filter((task) => task.done === true);
+    }
+    if (req.query.filterBy === "undone") {
+      tasks = tasks.filter((task) => task.done === false);
+    }
+    if (req.query.pp && req.query.page) {
+      const lastTaskIndex = req.query.page *  req.query.pp;
+      const firstTaskIndex = lastTaskIndex - req.query.pp;
+      tasks = tasks.slice(firstTaskIndex, lastTaskIndex);
+      console.log("Tasks per page: ", req.query.pp);
+      console.log("Current page: ", req.query.page);
+    }
 
-      }
-      if (req.query.filterBy) {
-        if (req.query.filterBy === 'done') {
-          tasks = tasks.filter(task => task.done === true)
-        } else { tasks = tasks.filter(task => !task.done === true) }
-      }
-
-      res.send({ count: tasks.length, tasks });
-    })
+    res.send({ count: tasks.length, tasks });
+  });
 });
 
-module.exports = router
+module.exports = router;
