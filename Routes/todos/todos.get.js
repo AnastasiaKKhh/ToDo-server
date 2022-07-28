78592b045ea52b151f158ab7c5c98c21940db96d
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-07-28 15:05:23 +0300

    Update get request

--- Routes/todos/todos.get.js
+++ Routes/todos/todos.get.js
@@ -1,27 +1,29 @@
 const c = require("config");
 const express = require("express");
 const router = express.Router();
-const db = require("../../models")
+const db = require("../../models");
 
 router.get("/todos", async function (req, res) {
   try {
-    const { query: { page=1, pp=5, filterBy, order='asc' } } = req;
+    const {
+      query: { page = 1, pp = 5, filterBy, order = "asc" },
+    } = req;
     let tasks;
 
     const FILTERS = {
       done: true,
-      undone: false
-    }
-    const whereCondition = filterBy ? { done: FILTERS[filterBy] } : {}
-   
-      tasks = await db.Task.findAndCountAll({
-        where: whereCondition,
-        order: [["createdAt", order]],
-        limit: pp,
-        offset: (+page - 1) * +pp
-      })
-    
-    const { rows, count } = tasks
+      undone: false,
+    };
+    const whereCondition = filterBy ? { done: FILTERS[filterBy] } : {};
+
+    tasks = await db.Task.findAndCountAll({
+      where: whereCondition,
+      order: [["createdAt", order]],
+      limit: pp,
+      offset: (+page - 1) * +pp,
+    });
+
+    const { rows, count } = tasks;
     return res.send({ count, tasks: rows });
   } catch (error) {
     return res.status(500).send(error);
