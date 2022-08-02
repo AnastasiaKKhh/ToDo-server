cce3fea57498adc644a567a3f81bf6130bc1464c
Анастасия <anastasiyakhokhlova22@gmail.com>
2022-08-02 13:30:32 +0300

    Delete extra code

--- Routes/users/users.get.js
+++ Routes/users/users.get.js
@@ -6,9 +6,7 @@ const jwt = require("jsonwebtoken");
 
 router.get("/users", async function (req, res) {
   try {
-    // const {params: { auth }} = req;
     const users = await db.User.findAll();
-    const token = jwt.sign({ foo: "bar" }, "shhhhh");
     return res.send(users);
   } catch (error) {
     return res.status(error.status || 500).send(error);
